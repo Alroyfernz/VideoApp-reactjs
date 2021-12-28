8415c475074f52d4abe450c91db8e3d68f2a2cd6
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-12-28 18:43:21 +0530

    done with socket.io server implementation

--- server/index.js
+++ server/index.js
@@ -12,7 +12,18 @@ const PORT = process.env.PORT || 8000;
 app.get("/", (req, res) => {
   res.send("server running");
 });
-
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+  socket.on("callUser", ({ userIoCall, signalData, from, name }) => {
+    io.to(userIoCall).emit("callUser", { signal: signalData, from, name });
+  });
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callaccepted", data.signal);
+  });
+});
 server.listen(PORT, () => {
   console.log("Server listening on port", PORT);
 });
