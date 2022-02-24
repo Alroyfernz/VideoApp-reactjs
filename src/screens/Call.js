f5af0c87b7e5ef3b4a059bad190b0a5488993510
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-02-24 22:35:03 +0530

    done with calling screen

--- src/screens/Call.js
+++ src/screens/Call.js
@@ -4,7 +4,8 @@ import { BsCameraVideo, BsCameraVideoOff } from "react-icons/bs";
 import { FaRegClosedCaptioning } from "react-icons/fa";
 import { HiDotsVertical } from "react-icons/hi";
 import { SocketContext } from "../SocketContext";
-
+import { MdContentCopy } from "react-icons/md";
+import { CgClose } from "react-icons/cg";
 import {
   MdInfoOutline,
   MdOutlinePeopleAlt,
@@ -14,14 +15,47 @@ import {
 } from "react-icons/md";
 import "./call.scss";
 const Call = () => {
+  const [closeId, setCloseId] = useState(false);
   const { call, callAccepted, myVideo, userVideo, stream, name, callEnded } =
     useContext(SocketContext);
   console.log(myVideo);
-  const secondUser = null;
+  const secondUser = "null";
   const [hideVideo, setHideVideo] = useState(false);
   return (
     <section className="callSection">
       <div className="callWrapper">
+        <div className="copyId" style={{ opacity: closeId ? 0 : 1 }}>
+          <div className="idWrapper">
+            <div className="top">
+              <span
+                style={{
+                  fontSize: 18,
+                  fontWeight: 400,
+                  color: "#444",
+                  textTransform: "capitalize",
+                }}
+              >
+                Your meeting is ready
+              </span>
+
+              <span>
+                <CgClose
+                  onClick={() => {
+                    setCloseId(!closeId);
+                  }}
+                  style={{ fontSize: 20, color: "#666", cursor: "pointer" }}
+                />
+              </span>
+            </div>
+            <div className="bottom">
+              <span>Share this id with others you want in the meeting.</span>
+              <div className="idBox">
+                12345
+                <MdContentCopy style={{ color: "#fff", fontSize: 18 }} />
+              </div>
+            </div>
+          </div>
+        </div>
         {secondUser == null ? (
           <div className="mainUser">
             <img
@@ -33,7 +67,6 @@ const Call = () => {
 
             <video
               playinline="true"
-              muted
               ref={myVideo}
               autoPlay
               style={{
@@ -60,7 +93,20 @@ const Call = () => {
                 src="https://yt3.ggpht.com/ytc/AKedOLQMxO5ybJytpgAsgyYDiMw2lrUpVGo1YZddOKEljQ=s900-c-k-c0x00ffffff-no-rj"
                 alt=""
                 className="userAvatar"
+                style={{ opacity: hideVideo ? 1 : 0 }}
               />
+
+              <video
+                playinline="true"
+                muted
+                ref={myVideo}
+                autoPlay
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  display: hideVideo ? "none" : "flex",
+                }}
+              ></video>
               <span className="userName">user name</span>
             </div>
             <div className="mainUser" style={{ width: "48%" }}>
@@ -68,7 +114,20 @@ const Call = () => {
                 src="https://yt3.ggpht.com/ytc/AKedOLQMxO5ybJytpgAsgyYDiMw2lrUpVGo1YZddOKEljQ=s900-c-k-c0x00ffffff-no-rj"
                 alt=""
                 className="userAvatar"
+                style={{ opacity: hideVideo ? 1 : 0 }}
               />
+
+              <video
+                playinline="true"
+                muted
+                ref={myVideo}
+                autoPlay
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  display: hideVideo ? "none" : "flex",
+                }}
+              ></video>
               <span className="userName">user name</span>
             </div>
           </section>
@@ -103,7 +162,12 @@ const Call = () => {
           <div className="iconsMain">
             <MdOutlineScreenShare className="optionsIcons" />
           </div>
-          <div className="iconsMain">
+          <div
+            className="iconsMain"
+            onClick={() => {
+              setCloseId(!closeId);
+            }}
+          >
             <HiDotsVertical className="optionsIcons" />
           </div>
           <div className="endCall">
