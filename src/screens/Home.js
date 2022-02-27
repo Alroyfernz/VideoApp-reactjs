e2d30dd2e05873f8bf938fcb78e3dd76c32d6718
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-02-28 00:29:56 +0530

    created call screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,6 +5,29 @@ import { MdOutlineVideoCall } from "react-icons/md";
 import "./Home.scss";
 import axios from "axios";
 import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+} from "@chakra-ui/react";
+import { FaMicrophone } from "react-icons/fa";
+import { BsCameraVideo, BsCameraVideoOff } from "react-icons/bs";
+import {
+  MdInfoOutline,
+  MdOutlinePeopleAlt,
+  MdOutlineChat,
+  MdCallEnd,
+  MdOutlineScreenShare,
+} from "react-icons/md";
+import { FaRegClosedCaptioning } from "react-icons/fa";
+import { HiDotsVertical } from "react-icons/hi";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { MdContentCopy } from "react-icons/md";
+import { CgClose } from "react-icons/cg";
+import {
   Modal,
   ModalOverlay,
   ModalContent,
@@ -12,7 +35,6 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  Button,
 } from "@chakra-ui/react";
 import {
   Menu,
@@ -30,19 +52,51 @@ import {
 import { BsKeyboard } from "react-icons/bs";
 import { SocketContext } from "../SocketContext";
 const Home = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
+  const [showCall, setShowCall] = useState(false);
   const navigation = useNavigate();
+  const [isOpen, setIsOpen] = React.useState(false);
+  const onClose = () => setIsOpen(false);
+  const cancelRef = React.useRef();
+
   const [isJoin, setIsJoin] = useState(false);
   const [user, setUser] = useState(null);
   const [showSpinner, setSpin] = useState(false);
   const callIdRef = useRef(null);
   const [showMod, setMod] = useState(false);
-  const { callAccepted, setName, callUser, stream, userVideo, myVideo } =
+  const [isPm, setIsPm] = useState("AM");
+  const [hr, setHr] = useState(0);
+  const [ans, setAns] = useState(false);
+  const [min, setMin] = useState(0);
+  const validateDate = () => {
+    var today = new Date();
+
+    setMin(today.getMinutes());
+    var date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate();
+    var time =
+      today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + " " + time;
+    console.log(dateTime);
+    if (today.getHours() > 12) {
+      setIsPm("PM");
+      const updateTime = today.getHours() - 12;
+      setHr(updateTime);
+    } else {
+      setIsPm("AM");
+      setHr(today.getHours());
+    }
+  };
+  const { callAccepted, setName, callUser, stream, me, userVideo, myVideo } =
     useContext(SocketContext);
   console.log(myVideo);
   // if (myVideo) localStorage.setItem("myVideo", JSON.stringify(myVideo));
   const handleCall = () => {
+    setShowCall(true);
+    console.log("how tf called?");
     console.log(callIdRef?.current.value);
     // if (callIdRef?.current.value == "") return;
     callUser(callIdRef?.current.value);
@@ -54,6 +108,7 @@ const Home = () => {
     }
   };
   useEffect(() => {
+    validateDate();
     const getUser = async () => {
       // fetch("http://localhost:8000/login/success", {
       //   method: "GET",
@@ -99,145 +154,220 @@ const Home = () => {
     }
   }, [navigation]);
 
-  if (showSpinner) {
-    return (
-      <div
-        className="spinner"
-        style={{
-          display: showSpinner ? "flex" : "none",
-          backgroundColor: "rgba(173, 216, 230, 0.1)",
-          flexDirection: "column",
-          width: "100vw",
-          height: "100vh",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="grau.500"
-          size="xl"
-        />
-        <h1
-          style={{ fontSize: 18, textTransform: "capitalize", marginTop: 10 }}
+  return (
+    <>
+      <Navbar />
+      <div className="heroSection">
+        <div
+          style={{
+            display: showCall ? "flex" : "none",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}
         >
-          wait until you are admitted...
-        </h1>
-      </div>
-    );
-  } else {
-    return (
-      <>
-        <Navbar />
-        <div className="heroSection">
-          <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalHeader>Video preview</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody></ModalBody>
+          <AlertDialog
+            isOpen={isOpen}
+            leastDestructiveRef={cancelRef}
+            onClose={onClose}
+          >
+            <AlertDialogOverlay>
+              <AlertDialogContent>
+                <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                  Incoming call
+                </AlertDialogHeader>
 
-              <ModalFooter>
-                <Button colorScheme="blue" mr={3} onClick={onClose}>
-                  Close
-                </Button>
-                <Button variant="ghost">Secondary Action</Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
-          <video
-            ref={myVideo}
-            playinline
-            autoPlay
-            muted
-            style={{
-              width: "300px",
-              height: "400px",
-              display: "none",
-            }}
-          />
-          <video
-            ref={userVideo}
-            playinline
-            autoPlay
-            muted
-            style={{
-              width: "300px",
-              height: "400px",
-              display: "none",
-            }}
-          />
-          <div className="heroWrapper">
-            <div className="heroLeft">
-              <h1>
-                Premium video calling <br />
-                Now free for everyone
-              </h1>
-              <p>
-                We re-engineered the service we built for secure business
-                meetings, Google Meet, to make it free and available for all.
-              </p>
-              <div className="bottomButtons">
-                <button className="createMeet">
-                  <MdOutlineVideoCall className="video" />
-                  <a href="/call">new meeting</a>
-                </button>
+                <AlertDialogBody style={{ textTransform: "capitalize" }}>
+                  alroy is trying to join...
+                </AlertDialogBody>
 
-                <div className="inputWrapper">
-                  <div className="inputMeeting" style={{ width: "70%" }}>
-                    <BsKeyboard className="keyboard" />
-                    <input
-                      type="text"
-                      placeholder="Enter a code"
-                      onFocus={() => {
-                        setIsJoin(true);
-                      }}
-                      ref={callIdRef}
-                      onBlur={() => {
-                        setIsJoin(false);
-                      }}
-                    />
-                  </div>
-                  <button
-                    className="joinBtn"
-                    onClick={handleCall}
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                    }}
-                  >
-                    Join
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="heroRight">
-              <div className="rightWrapper">
-                <div>
-                  <img
-                    src="https://www.gstatic.com/meet/user_edu_get_a_link_light_90698cd7b4ca04d3005c962a3756c42d.svg"
-                    alt=""
+                <AlertDialogFooter>
+                  <Button colorScheme="red" ref={cancelRef} onClick={onClose}>
+                    Reject
+                  </Button>
+                  <Button ml={3}>Accept</Button>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialogOverlay>
+          </AlertDialog>
+          <div className="copyId">
+            <div className="idWrapper">
+              <div className="top">
+                <span
+                  style={{
+                    fontSize: 18,
+                    fontWeight: 400,
+                    color: "#444",
+                    textTransform: "capitalize",
+                  }}
+                >
+                  Your meeting is ready
+                </span>
+
+                <span>
+                  <CgClose
+                    style={{ fontSize: 20, color: "#666", cursor: "pointer" }}
                   />
-                  <h3>Get a code you can share</h3>
-                  <p>
-                    Click{" "}
-                    <span style={{ fontWeight: 500, color: "#000" }}>
-                      New meeting{" "}
-                    </span>{" "}
-                    to get a code which you can ahre with the people you want to
-                    meet
-                  </p>
+                </span>
+              </div>
+              <div className="bottom">
+                <span>Share this id with others you want in the meeting.</span>
+                <div className="idBox">
+                  {me}
+                  <CopyToClipboard text={me}>
+                    <MdContentCopy
+                      style={{ color: "#fff", fontSize: 18, cursor: "pointer" }}
+                    />
+                  </CopyToClipboard>
                 </div>
               </div>
             </div>
           </div>
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <video
+              ref={myVideo}
+              playinline
+              autoPlay
+              muted
+              style={{
+                borderRadius: 9,
+                display: !showCall ? "none" : "block",
+              }}
+            />
+            <video
+              playinline
+              src="https://youtu.be/5qap5aO4i9A"
+              autoPlay
+              muted
+            />
+          </div>
+          <div className="bottomNavigation">
+            <div className="time">
+              <h3>
+                {hr}:{min} {isPm} | gdb-vm-cu
+              </h3>
+            </div>
+            <div className="options">
+              <div className="iconsMain">
+                <FaMicrophone
+                  className="optionsIcons"
+                  stlye={{ fontSize: 16 }}
+                />
+              </div>
+              <div className="iconsMain">
+                <BsCameraVideo className="optionsIcons" />
+              </div>
+              <div className="iconsMain">
+                <FaRegClosedCaptioning className="optionsIcons" />
+              </div>
+              <div className="iconsMain">
+                <MdOutlineScreenShare className="optionsIcons" />
+              </div>
+              <div className="iconsMain">
+                <HiDotsVertical className="optionsIcons" />
+              </div>
+              <div className="endCall">
+                <MdCallEnd
+                  style={{ color: "#fff", fontSize: 20 }}
+                  onClick={() => {
+                    setShowCall(false);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="right">
+              <MdInfoOutline className="rightIcons" />
+              <MdOutlinePeopleAlt className="rightIcons" />
+
+              <MdOutlineChat className="rightIcons" />
+            </div>
+          </div>
         </div>
-      </>
-    );
-  }
+
+        <div
+          className="heroWrapper"
+          style={{ display: showCall ? "none" : "flex" }}
+        >
+          <div className="heroLeft">
+            <h1>
+              Premium video calling <br />
+              Now free for everyone
+            </h1>
+            <p>
+              We re-engineered the service we built for secure business
+              meetings, Google Meet, to make it free and available for all.
+            </p>
+            <div className="bottomButtons">
+              <button
+                className="createMeet"
+                onClick={() => {
+                  setShowCall(true);
+                }}
+              >
+                <MdOutlineVideoCall className="video" />
+                <a>new meeting</a>
+              </button>
+
+              <div className="inputWrapper">
+                <div className="inputMeeting" style={{ width: "70%" }}>
+                  <BsKeyboard className="keyboard" />
+                  <input
+                    type="text"
+                    placeholder="Enter a code"
+                    onFocus={() => {
+                      setIsJoin(true);
+                    }}
+                    ref={callIdRef}
+                    onBlur={() => {
+                      setIsJoin(false);
+                    }}
+                  />
+                </div>
+                <button
+                  className="joinBtn"
+                  onClick={handleCall}
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  Join
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="heroRight">
+            <div className="rightWrapper">
+              <div>
+                <img
+                  src="https://www.gstatic.com/meet/user_edu_get_a_link_light_90698cd7b4ca04d3005c962a3756c42d.svg"
+                  alt=""
+                />
+                <h3>Get a code you can share</h3>
+                <p>
+                  Click{" "}
+                  <span style={{ fontWeight: 500, color: "#000" }}>
+                    New meeting{" "}
+                  </span>{" "}
+                  to get a code which you can ahre with the people you want to
+                  meet
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
